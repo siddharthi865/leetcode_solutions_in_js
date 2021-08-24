f5006ae1aab409544c4bac5a3f4e0c34d682d8f6
Siddharth <siddharthi865@gmail.com>
2021-08-24 17:30:18 +0530

    added

---
+++ 11_container_with_most_water/container1.test.js
@@ -0,0 +1,17 @@
+const maxArea = require("./container1");
+
+test("Maximum area that can be filled is", () => {
+  expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
+});
+
+test("Maximum area that can be filled is", () => {
+  expect(maxArea([1, 1])).toBe(1);
+});
+
+test("Maximum area that can be filled is", () => {
+  expect(maxArea([4, 3, 2, 1, 4])).toBe(16);
+});
+
+test("Maximum area that can be filled is", () => {
+  expect(maxArea([1, 2, 1])).toBe(2);
+});
