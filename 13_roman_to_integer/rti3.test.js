186795bfd87269291ca93e881684a6c02dcf1bf5
Siddharth <siddharthi865@gmail.com>
2021-08-24 21:13:46 +0530

    added

---
+++ 13_roman_to_integer/rti3.test.js
@@ -0,0 +1,21 @@
+const romanToInt = require("./rti3");
+
+test("III converted to integer would be 3", () => {
+  expect(romanToInt("III")).toBe(3);
+});
+
+test("4 converted to integer would be IV", () => {
+  expect(romanToInt("IV")).toBe(4);
+});
+
+test("9 converted to integer would be IX", () => {
+  expect(romanToInt("IX")).toBe(9);
+});
+
+test("58 converted to integer would be LVIII", () => {
+  expect(romanToInt("LVIII")).toBe(58);
+});
+
+test("1994 converted to integer would be MCMXCIV", () => {
+  expect(romanToInt("MCMXCIV")).toBe(1994);
+});
