20dbab82ab207a1f4c2dfcc5a8b667a80f075518
Siddharth <siddharthi865@gmail.com>
2021-08-24 23:20:49 +0530

    added

---
+++ 14_longest_common_prefix/lcp1.test.js
@@ -0,0 +1,9 @@
+const longestCommonPrefix = require("./lcp1");
+
+test('Longest Common Prefix in ["flower","flow","flight"] is ["fl"]', () => {
+  expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe("fl");
+});
+
+test('Longest Common Prefix in ["dog","racecar","car"] is ""', () => {
+  expect(longestCommonPrefix(["dog", "racecar", "car"])).toBe("");
+});
---
+++ 15_3sum/3sum1.test.js
@@ -0,0 +1,16 @@
+const threeSum = require("./3sum1");
+
+test("3sum of  [-1,0,1,2,-1,-4] is [[-1,-1,2],[-1,0,1]]", () => {
+  expect(threeSum([-1, 0, 1, 2, -1, -4])).toStrictEqual([
+    [-1, -1, 2],
+    [-1, 0, 1],
+  ]);
+});
+
+test("Longest Common Prefix in [] is []", () => {
+  expect(threeSum([])).toStrictEqual([]);
+});
+
+test("Longest Common Prefix in [0] is []", () => {
+  expect(threeSum([0])).toStrictEqual([]);
+});
