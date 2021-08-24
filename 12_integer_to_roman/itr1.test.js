650ca75f750372c8df027d77be6eff5ba762dcb4
Siddharth <siddharthi865@gmail.com>
2021-08-24 17:44:51 +0530

    added

---
+++ 12_integer_to_roman/itr1.test.js
@@ -0,0 +1,21 @@
+const intToRoman = require("./itr1");
+
+test("3 converted to roman would be III", () => {
+  expect(intToRoman(3)).toBe("III");
+});
+
+test("4 converted to roman would be IV", () => {
+  expect(intToRoman(4)).toBe("IV");
+});
+
+test("9 converted to roman would be IX", () => {
+  expect(intToRoman(9)).toBe("IX");
+});
+
+test("58 converted to roman would be LVIII", () => {
+  expect(intToRoman(58)).toBe("LVIII");
+});
+
+test("1994 converted to roman would be MCMXCIV", () => {
+  expect(intToRoman(1994)).toBe("MCMXCIV");
+});
