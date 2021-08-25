2fec15102b42f60ed70730cdb16ceb3987ab0143
Siddharth <siddharthi865@gmail.com>
2021-08-25 08:47:22 +0530

    added

---
+++ 16_3sum_closest/3sumclosest1.test.js
@@ -0,0 +1,9 @@
+const isValid = require("./3sumclosest1");
+
+test("The sum that is closest to the target is 2", () => {
+  expect(isValid([-1, 2, 1, -4], 1)).toBe(2);
+});
+
+test("The sum that is closest to the target is 0", () => {
+  expect(isValid([0, 0, 0], 1)).toBe(0);
+});
---
+++ 16_3sum_closest/3sumclosest2.js
@@ -0,0 +1,20 @@
+var threeSumClosest = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  let min = Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    let left = i + 1,
+      right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(sum - target) < Math.abs(min - target)) min = sum;
+      if (sum > target) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+  return min;
+};
+
+module.exports = threeSumClosest;
